2f33e62cb44647b1fbb9f6b73d4b61902aaf04fd
Pestco <158598021+Pestco@users.noreply.github.com>
2024-02-04 16:14:15 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,33 @@
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("result");
+    status.innerHTML = "Please wait..."
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.status == 200) {
+            status.innerHTML = responsePostsubmit;
+            alert(alertPostSubmit);
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "There was a problem submitting your form"
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
+
